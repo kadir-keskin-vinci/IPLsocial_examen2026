bcffca665fbd80981535843e273e8d7c212ac780
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-01-16 17:08:00 +0100

    refactor: factorise EmailChecker clas instaction in tests

--- EmailChecker.test.ts
+++ EmailChecker.test.ts
@@ -2,15 +2,16 @@ import { EmailChecker } from "./EmailChecker";
 
 describe("EmailCheck", () => {
   describe("verif method", () => {
+     const emailChecker = new EmailChecker();
     it("should return false when given don't contains @", () => {
-      const emailChecker = new EmailChecker();
+      
       const actual = emailChecker.verif("lolo");
       expect(actual).toBe(false);
     });
     
       it("should return false when given  don't contains dot in domains", () =>{
             
-      const emailChecker = new EmailChecker();
+     
       const actual = emailChecker.verif("lolo@domainecom");
       expect(actual).toBe(false);
     })
